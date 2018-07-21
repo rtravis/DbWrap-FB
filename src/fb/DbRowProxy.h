eb8ab83347918bb3a7a5a549ce711138bc1b4f48
massmarz <massimo.marzano@gmail.com>
2018-07-21 09:47:31 +0200

    Solved global namespace pollution due to ibase.h inclusion

--- src/fb/DbRowProxy.h
+++ src/fb/DbRowProxy.h
@@ -20,11 +20,7 @@
 #include <string>
 #include "FbCommon.h"
 
-#include "FbInternals.h"
-#include "FbException.h"
 #include <memory>
-#include <time.h>
-#include <iomanip>
 #include <unordered_map>
 
 namespace fb {
@@ -51,6 +47,8 @@ public:
     std::string getText(unsigned int idx) const;
     DbBlob getBlob(unsigned int idx) const;
     DbFieldPtr fieldByName(const std::string& name);
+    std::string formatDate(unsigned int idx, const std::string &format = "%Y-%m-%d") const;
+
 private:
     std::unordered_map<std::string, DbFieldPtr> fields;
     DbRowProxy(SqlDescriptorArea *sqlda, FbApiHandle db, FbApiHandle tr);
@@ -65,11 +63,10 @@ class DbField {
     //friend class DbRowProxy;
 private:
     DbRowProxy* rowProxy;
-    SqlDescriptorArea *row_;
     unsigned int idx;
 public:
-    DbField(DbRowProxy* rowProxy, SqlDescriptorArea *row, unsigned int idx) 
-    :rowProxy(rowProxy), row_(row), idx(idx){};
+    DbField(DbRowProxy* rowProxy, unsigned int idx) 
+    :rowProxy(rowProxy), idx(idx){};
 
     int64_t asInteger() {
         return rowProxy->getInt64(idx);
@@ -84,25 +81,7 @@ public:
     }
 
     std::string formatDate(const std::string &format = "%Y-%m-%d") {
-
-        struct tm times;
-        const XSQLVAR &v1 = row_->sqlvar[idx];
-        
-        switch (v1.sqltype & ~1){
-            case SQL_TYPE_DATE:
-                isc_decode_sql_date((ISC_DATE *) v1.sqldata, &times);
-                break;
-            case SQL_TIMESTAMP:
-                isc_decode_timestamp((ISC_TIMESTAMP *) v1.sqldata, &times);
-                break;
-            default:
-            throw FbException("Incompatible types.", nullptr);
-        }
-
-        char buff[30];
-        strftime(buff, 30, format.c_str(), &times);
-
-        return buff;
+        return rowProxy->formatDate(idx, format);
     }
 };
 
